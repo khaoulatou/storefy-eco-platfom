0e6614c42dac5bd21bc67d76beb10d0ff6c92ac8
othmanekahtal <othmanekahtal@gmail.com>
2021-09-27 16:10:38 +0100

    clean up routes and add resend route

---
+++ back-end/routes/api.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\PixelController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\SocialAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['prefix' => 'v1'], function () {
+
+    // authentication routes :
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/forget', [ResetController::class, 'forgotPassword']);
+    Route::post('/reset', [ResetController::class, 'reset']);
+
+    // country api :
+    Route::get('/country', [CountryController::class, 'getAllCountry']);
+    Route::post('/create-country', [CountryController::class, 'createCountry']);
+    // authentication with social media :
+    Route::get('/authorize/{provider}/redirect', [SocialAuthController::class, 'redirectToProvider']);
+    Route::get('/authorize/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
+
+    // email activation :
+    Route::post('resend', [EmailVerificationController::class, 'resendVerificationEmail'])->name('verification.resend');
+    Route::get('verify-email', [EmailVerificationController::class, 'verify'])->name('verification.verify');
+
+
+    // will be authenticated :
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        // send activation email :
+        Route::post('email/verification-notification', [EmailVerificationController::class, 'sendVerificationEmail'])->name('verification.send');
+
+        // pixel functionality :
+        Route::post('pixel/{id}', [PixelController::class, 'update']);
+        Route::resource('pixel', PixelController::class)->except(['create', 'edit']);
+
+        // logout
+        Route::post('/logout', [AuthController::class, 'logout']);
+
+        // update profile router :
+//        Route::post('/profile', [UserAuthentication::class, 'profile']);
+    });
+
+});
