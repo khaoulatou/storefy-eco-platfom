2a1f254668b523b78b78026aa618e18a6930f67a
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-08 18:05:14 +0100

    update coupon model

---
+++ back-end/app/Models/Coupon.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        "nom",
+        "active",
+        "dateDebut",
+        "dateFin",
+        "nombre_utilisateur",
+        "type_prix",
+        "type_expiration",
+        "subdomain"
+    ];
+}
