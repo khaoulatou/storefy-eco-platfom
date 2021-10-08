35c57c9dbebda1fb60a1993e3dc54935d615bd85
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-08 18:07:28 +0100

    update Produit controller

--- back-end/app/Http/Controllers/ProduitController.php
+++ back-end/app/Http/Controllers/ProduitController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Produit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProduitController extends Controller
 {
@@ -69,7 +70,8 @@ class ProduitController extends Controller
      */
     public function update(Request $request, Produit $produit)
     {
-        //
+       
+        // return response(['success' => true, 'message' => 'update product succesfuly !', 'data' => $produit]);
     }
 
     /**
@@ -81,5 +83,22 @@ class ProduitController extends Controller
     public function destroy(Produit $produit)
     {
         return Produit::destroy($produit);
+    }
+    //get Produits By Commande
+    public function getProduitsByCommande($id)
+    {
+        $produit = DB::table('produits')->distinct()
+            ->join('produit_commandes', 'produits.id', '=', 'produit_commandes.produit_id')
+            ->join('commandes', 'commandes.id', '=', 'produit_commandes.commande_id')
+            ->select('produit_commandes.*')->where('commandes.id', '=', $id)
+            ->get();
+        if (is_null($produit)) {
+            return $this->sendError('Products not found.');
+        }
+        return response([
+            "success" => true,
+            "message" => "List of products to order.",
+            "data" => $produit
+        ], 201);
     }
 }
