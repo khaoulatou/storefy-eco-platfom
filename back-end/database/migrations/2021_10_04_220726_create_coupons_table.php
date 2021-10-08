c8ec310ef5da8707a047b41b230a69dc0042e3d2
Mohssine Elattari <elattari.mohssine@gmail.com>
2021-10-08 18:09:04 +0100

    add migrate coupon

---
+++ back-end/database/migrations/2021_10_04_220726_create_coupons_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->boolean('active');
+            $table->date('dateDebut');
+            $table->date('dateFin');
+            $table->integer('capacity');//2000
+            $table->integer('nombre_utilisateur');//2000
+            $table->string('type_prix');
+            $table->string('type_expiration');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
